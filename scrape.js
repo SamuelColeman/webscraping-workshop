6a808fa613d8d9a7d8fd26081f772a8d64fc9983
Samuel Coleman <smcoleman19@gmail.com>
2019-11-13 17:13:50 -0700

    Add functionality for navigating to volunteer football page

--- scrape.js
+++ scrape.js
@@ -2,11 +2,18 @@ var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: true });
 
 nightmare
-  .goto('https://www.secsports.com/clubhouse/football')
-  .click('a[href="https://www.secsports.com/scores/football"]')
-  .wait('.status')
+  .goto('https://www.secsports.com/')
+  .wait('#navigation-tier2-sports-dropdown-toggle')
+  .click('#navigation-tier2-sports-dropdown-toggle')
+  .wait('a[href="/clubhouse/football"]')
+  .click('a[href="/clubhouse/football"]')
+  .wait('#subnav-teams-dropdown-toggle')
+  .click('#subnav-teams-dropdown-toggle')
+  .wait('a[href="/clubhouse/football/tennessee-volunteers"]')
+  .click('a[href="/clubhouse/football/tennessee-volunteers"]')
+  .wait('.season-line')
   .evaluate(function () {
-    var nameNodes = document.querySelectorAll('.status');
+    var nameNodes = document.querySelectorAll('.season-line');
     var list = [].slice.call(nameNodes);
     return list.map(function(node){
       return node.innerText
